c9b42e476c6d10f25337c2adfe4dd00276e6e6ce
Stevility <65314652+Stevility@users.noreply.github.com>
2022-10-21 14:00:32 -0400

    "AK-47 will now animate at the speed of the fire rate"

--- Assets/Scripts/gunPickup.cs
+++ Assets/Scripts/gunPickup.cs
@@ -6,6 +6,15 @@ public class gunPickup : MonoBehaviour
 {
     [SerializeField] GunStats gunStat;
 
+    //Gun Rotation
+    float x;
+
+    private void Update()
+    {
+        x += Time.deltaTime * 10;
+        transform.rotation = Quaternion.Euler(0, x, 0);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && gameManager.instance.playerScript != null)
